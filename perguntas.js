46ff2b35530214bf7e06d7a431150cd2d0bba398
Junior Festa <everson.festa.junior@escola.pr.gov.br>
2025-07-28 11:37:39 -0300

    gjg

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    Futebol,
+    Quantas champions o maior campeão tem?,
+    Real Madrid, 15 champions
+)
+criaCartao(
+    Fubebol,
+    Qual seleção que mais possui copa do mundo e quantas?,
+    Brasil, 5 copa do mundo, Pentacampeão.
+)
+criaCartao(
+    Futebol,
+    Qual o time com mais Mundiais de clubes da história?,
+    Real Madrid, 9 vezes campeões.
+)
